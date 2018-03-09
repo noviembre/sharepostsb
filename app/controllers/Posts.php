0bcac2d467311b10e9c1b1eb57e141233ea54b72
Artemis Fowl <realgarcilaso@outlook.com>
2018-03-09 06:12:50 -0500

    07 eliminando un posts solo si el propietario es el dueno del post

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -142,4 +142,26 @@ class Posts extends Controller {
         $this->view('posts/show', $data);
     }
 
+    public function delete($id){
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            // Get existing post from model
+            $post = $this->postModel->getPostById($id);
+
+            // si el post q se va borrar pertenece al que
+            // inicio sesion entonce...que lo borre
+            if($post->user_id != $_SESSION['user_id']){
+                redirect('posts');
+            }
+
+            if($this->postModel->deletePost($id)){
+                flash('post_message', 'Post Removed');
+                redirect('posts');
+            } else {
+                die('Something went wrong');
+            }
+        } else {
+            redirect('posts');
+        }
+    }
+
 }
